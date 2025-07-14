c25f39fe612db06e9cdddb70f5a94e0b5f4c83b0
SHIN-HOON-SIK <hoonsik128@gmail.com>
2025-07-14 12:31:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="SEMunion Chatbot", layout="centered")
 st.markdown(
     """
     <div style='display: flex; align-items: center; margin-bottom: 30px;'>
-        <img src='https://raw.githubusercontent.com/SHIN-HOON-SIK/semunion-chatbot/main/1.png' width='80' style='margin-right: 12px;'/>
+        <img src='https://raw.githubusercontent.com/SHIN-HOON-SIK/semunion-chatbot/main/1.png' width='60' style='margin-right: 12px;'/>
         <h1 style='margin: 0; font-size: 30px;'>삼성전기 존중노동조합 상담사</h1>
     </div>
     """,
