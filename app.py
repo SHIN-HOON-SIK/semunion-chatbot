b0c72806b57581e73c8bcc6d30a5e8d9b6186d19
SHIN-HOON-SIK <hoonsik128@gmail.com>
2025-07-14 12:37:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,18 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 
+st.markdown(
+    """
+    <hr style='margin-top: 50px; margin-bottom: 10px;'>
+
+    <div style='font-size: 13px; color: #888; line-height: 1.6;'>
+        주소: 수원시 영통구 매영로 159번길 19, 광교 더 퍼스트 지식산업센터<br>
+        사업자등록번호: 133-82-71927 | 대표: 신훈식<br>
+        대표번호: 010-9496-6517 | 이메일: hoonsik79@hanmail.net
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
 st.title("※노조 전문 상담사")
 st.write("안녕하세요! 여기에 질문을 입력하면, 노조 및 회사생활 관련 내용에 대해 자동으로 답변을 제공합니다.")
