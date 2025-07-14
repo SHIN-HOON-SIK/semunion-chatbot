1de4eff0235f0e604dfa9c135f1e9960899dfb19
SHIN-HOON-SIK <hoonsik128@gmail.com>
2025-07-14 10:46:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="SEMunion Chatbot", layout="centered")
 
-st.title("삼성전기 노조 전용 챗봇")
-st.write("안녕하세요! 여기에 질문을 입력하면, 노조 관련 자료에서 자동으로 답변을 제공합니다.")
+st.title("삼성전기 존중노동조합 전용 챗봇")
+st.write("안녕하세요! 여기에 질문을 입력하면, 노조 및 회사 생활에 관련된 내용에 자동으로 답변을 제공합니다.")
