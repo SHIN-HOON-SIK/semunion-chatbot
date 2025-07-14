4222e6d2f7505c157f5d733dc1822eda110ce37b
SHIN-HOON-SIK <hoonsik128@gmail.com>
2025-07-14 11:08:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="SEMUNION 상담사 ", layout="centered")
+st.set_page_config(page_title="SEMunion Chatbot", layout="centered")
 
 st.markdown("""
     <div style='text-align: center; margin-bottom: 10px;'>
@@ -8,5 +8,5 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 
-  st.title("삼성전기 존중노동조합 상담사")
-st.write("안녕하세요! 여기에 질문을 입력하면, 노조 및 회사 생활에 관련된 내용에 자동으로 답변을 제공합니다.")
+st.title("삼성전기 존중노동조합 상담사")
+st.write("안녕하세요! 여기에 질문을 입력하면, 노조 관련 자료에서 자동으로 답변을 제공합니다.")
