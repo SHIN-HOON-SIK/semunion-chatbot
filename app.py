6923a3be77e8555b16a3e35b8d2636a617591193
SHIN-HOON-SIK <hoonsik128@gmail.com>
2025-07-14 12:32:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="SEMunion Chatbot", layout="centered")
 
-st.markdown(
-    """
-    <div style='display: flex; align-items: center; margin-bottom: 30px;'>
-        <img src='https://raw.githubusercontent.com/SHIN-HOON-SIK/semunion-chatbot/main/1.png' width='60' style='margin-right: 12px;'/>
-        <h1 style='margin: 0; font-size: 30px;'>삼성전기 존중노동조합 상담사</h1>
+st.markdown("""
+    <div style='text-align: center; margin-bottom: 10px;'>
+        <img src='https://raw.githubusercontent.com/SHIN-HOON-SIK/semunion-chatbot/main/2.png' width='320'/>
     </div>
-    """,
-    unsafe_allow_html=True
-)
+""", unsafe_allow_html=True)
 
+st.title("삼성전기 존중노동조합 상담사")
 st.write("안녕하세요! 여기에 질문을 입력하면, 노조 관련 자료에서 자동으로 답변을 제공합니다.")
